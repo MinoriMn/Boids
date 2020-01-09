12b329cdbd7196f19ae86a39332f4ea2c3bec38e
MinoriMn <lucky_bluebrowneye10280915@au.com>
2020-01-09 15:54:05 +0900

    Boids完成

--- src/Boid.kt
+++ src/Boid.kt
@@ -1,7 +1,106 @@
+import app_display_manager.W_SIZE
 import processing.core.PVector
+
 
 //position: 位置座標, velocity:速度, acceleration:加速度
 data class Boid(var position: PVector, var velocity: PVector, var acceleration: PVector)
 const val BOID_BODY_SIZE = 2f
-const val BOID_MAX_SPEED = 0.5f
+const val BOID_MAX_FORCE = 0.03f
+const val BOID_MAX_SPEED = 2f
 const val BOID_AMOUNT = 100
+
+class BoidBehaviour{
+    companion object{
+        //分離(他のBoidオブジェクトと接触しないように距離を取る)
+        fun separate(me:Boid, others: MutableList<Boid>): PVector {
+            val desiredSeparation = 25.0f //距離25程度の間は空けたい
+            val steer = PVector(0f, 0f, 0f)
+            var count = 0 //近すぎると判断した個体数
+            for (other in others) {
+                val d = PVector.dist(me.position, other.position)
+                //距離が近い
+                if (d < desiredSeparation && d > 0) {
+                    val diff = PVector.sub(me.position, other.position)//離れるベクトル
+                    diff.normalize()//正規化
+                    diff.div(d)//距離が遠いほど小さくなる
+                    steer.add(diff)
+                    count++
+                }
+            }
+            if (count > 0) {
+                steer.div(count.toFloat())//平均化
+            }
+            if (steer.mag() > 0) {
+                steer.normalize()
+                steer.mult(BOID_MAX_SPEED)
+                steer.sub(me.velocity)
+                steer.limit(BOID_MAX_FORCE)
+            }
+            return steer
+        }
+
+        //整列(他のBoidオブジェクトと同じ方向を向くように修正)
+        fun align(me:Boid, boids: MutableList<Boid>): PVector {
+            val neighborDist = 50f //この距離の中で整列を試みる。
+            val sum = PVector(0f, 0f, 0f)
+            var count = 0
+            for (other in boids) {
+                val d = PVector.dist(me.position, other.position)
+                if (d > 0 && d < neighborDist) {
+                    sum.add(other.velocity)
+                    count++
+                }
+            }
+            return if (count > 0) {
+                sum.div(count.toFloat())
+                sum.normalize()
+                sum.mult(BOID_MAX_SPEED)
+                val steer = PVector.sub(sum, me.velocity)
+                steer.limit(BOID_MAX_FORCE)
+                steer
+            } else {
+                PVector(0f, 0f, 0f)
+            }
+        }
+
+        //結合(群の中心に向かう)
+        fun cohesion(me:Boid, boids: MutableList<Boid>): PVector {
+            val neighborDist = 50f //この半径距離の中で群の中心を探す
+            val sum = PVector(0f, 0f, 0f)
+            var count = 0
+            for (other in boids) {
+                val d = PVector.dist(me.position, other.position)
+                if (d < neighborDist && d > 0) {
+                    sum.add(other.position)
+                    count++
+                }
+            }
+            return if (count > 0) {
+                sum.div(count.toFloat())
+                seek(me, sum)
+            } else {
+                PVector(0f, 0f, 0f)
+            }
+        }
+        private fun seek(me:Boid, target: PVector): PVector {
+            val desired = PVector.sub(target, me.position)
+            desired.normalize()
+            desired.mult(BOID_MAX_SPEED)
+            val steer = PVector.sub(desired, me.velocity)
+            steer.limit(BOID_MAX_FORCE)
+            return steer
+        }
+
+        //位置情報更新
+        fun update(me:Boid): Unit {
+            me.velocity.add(me.acceleration).limit(BOID_MAX_SPEED)
+            me.position.add(me.velocity)
+            me.acceleration.mult(0f)
+
+            //境界線
+            if (me.position.x > W_SIZE) {me.position.x = -W_SIZE} else if (me.position.x < -W_SIZE) {me.position.x = W_SIZE}
+            if (me.position.y > W_SIZE) {me.position.y = -W_SIZE} else if (me.position.y < -W_SIZE) {me.position.y = W_SIZE}
+            if (me.position.z > W_SIZE) {me.position.z = -W_SIZE} else if (me.position.z < -W_SIZE) {me.position.z = W_SIZE}
+        }
+    }
+}
--- src/app_display_manager/AppDisplayManager.kt
+++ src/app_display_manager/AppDisplayManager.kt
@@ -1,25 +1,31 @@
 package app_display_manager
 
 import BOID_AMOUNT
+import BOID_BODY_SIZE
 import BOID_MAX_SPEED
 import Boid
+import BoidBehaviour.Companion.align
+import BoidBehaviour.Companion.cohesion
+import BoidBehaviour.Companion.separate
+import BoidBehaviour.Companion.update
 import processing.core.PApplet
 import processing.core.PConstants
 import processing.core.PVector
 
 
 //メインウィンドウ
-const val WINDOW_WIDTH = 920
-const val WINDOW_HEIGHT = 600
+const val WINDOW_WIDTH = 920f
+const val WINDOW_HEIGHT = 600f
 
-//
+//ワールド設定
+const val W_SIZE = 300f //立方体を想定。その中心を0としたときの面までの最短距離(つまり一辺の半分の長さ)
 
 class AppDisplayManager : PApplet (){
     fun run(args: Array<String>) : Unit = PApplet.main(AppDisplayManager::class.qualifiedName) //processing起動
 
     override fun settings(){
         //TIS = Text Input Sources, TSM = Text Services Manager
-        size(WINDOW_WIDTH, WINDOW_HEIGHT, PConstants.P3D)
+        size(WINDOW_WIDTH.toInt(), WINDOW_HEIGHT.toInt(), PConstants.P3D)
         //fullScreen(PConstants.P2D)
     }
 
@@ -29,41 +35,84 @@ class AppDisplayManager : PApplet (){
 
     override fun setup(){
         fill(0xff)
+        stroke(0xff)
         frameRate(60f)
 
         hint(PConstants.DISABLE_DEPTH_TEST)
         hint(PConstants.DISABLE_OPENGL_ERRORS)
 
+        //boids初期化
         boids = mutableListOf()
+        val r = 30f
         for (i in 0..BOID_AMOUNT){
-            val position = PVector(random(-W_SIZE, W_SIZE), random(-W_SIZE, W_SIZE), random(-W_SIZE, W_SIZE))
+            val position = PVector(random(-r, r), random(-r, r), random(-r, r))
             val velocity = PVector(random(-BOID_MAX_SPEED, BOID_MAX_SPEED), random(-BOID_MAX_SPEED, BOID_MAX_SPEED), random(-BOID_MAX_SPEED, BOID_MAX_SPEED))
             val acceleration = PVector(0f, 0f, 0f)
 
             boids.add(Boid(position, velocity, acceleration))
         }
-
     }
 
-    var rotX = 0f
-    var rotY = 0f
-    var rotZ = 0f
-
     override fun draw(){
+        boidsUpdate()
+
         background(0)
         translate(WINDOW_WIDTH/2f, WINDOW_HEIGHT/2f)
-        //X軸を中心に回転
-        rotateX(rotX)
-        //Y軸を中心に回転
-        rotateY(rotY)
-        //Z軸を中心に回転
-        rotateZ(rotZ)
-        //四角形を描く
-        rect(0f,0f,200f,200f)
-        //それぞれの軸の回転角度を更新
-        rotX += 0.02f
-        rotY += 0.03f
-        rotZ += 0.05f
+        rotateX(map(mouseY.toFloat(), 0f, WINDOW_HEIGHT, -HALF_PI, HALF_PI))
+        rotateY(map(mouseX.toFloat(), 0f, WINDOW_WIDTH, -HALF_PI, HALF_PI))
+
+        //境界線表示
+        line(-W_SIZE, -W_SIZE, -W_SIZE, W_SIZE, -W_SIZE, -W_SIZE)
+        line(W_SIZE, -W_SIZE, -W_SIZE, W_SIZE, -W_SIZE, W_SIZE)
+        line(W_SIZE, -W_SIZE, W_SIZE, -W_SIZE, -W_SIZE, W_SIZE)
+        line(-W_SIZE, -W_SIZE, W_SIZE, -W_SIZE, -W_SIZE, -W_SIZE)
+
+        line(-W_SIZE, W_SIZE, -W_SIZE, W_SIZE, W_SIZE, -W_SIZE)
+        line(W_SIZE, W_SIZE, -W_SIZE, W_SIZE, W_SIZE, W_SIZE)
+        line(W_SIZE, W_SIZE, W_SIZE, -W_SIZE, W_SIZE, W_SIZE)
+        line(-W_SIZE, W_SIZE, W_SIZE, -W_SIZE, W_SIZE, -W_SIZE)
+
+        line(-W_SIZE, -W_SIZE, -W_SIZE, -W_SIZE, W_SIZE, -W_SIZE)
+        line(W_SIZE, -W_SIZE, -W_SIZE, W_SIZE, W_SIZE, -W_SIZE)
+        line(W_SIZE, -W_SIZE, W_SIZE, W_SIZE, W_SIZE, W_SIZE)
+        line(-W_SIZE, -W_SIZE, W_SIZE, -W_SIZE, W_SIZE, W_SIZE)
+
+        boidsRender()
+    }
+    //Boidsの更新
+    private fun boidsUpdate() {
+        //加速度更新
+       boids.forEach{
+           val sep: PVector = separate(it, boids) //分離
+           val ali: PVector = align(it, boids) //整列
+           val coh: PVector = cohesion(it, boids) //結合
+           //パラメータ調整
+           sep.mult(1.5f)
+           ali.mult(1.0f)
+           coh.mult(1.0f)
+           it.acceleration.add(sep).add(ali).add(coh)
+       }
+
+        //position更新
+        boids.forEach{
+            update(it)
+        }
+    }
+
+    //Boidの描画
+    private fun boidsRender(): Unit {
+        boids.forEach {
+            pushMatrix()
+            translate(it.position.x, it.position.y, it.position.z)
+            rotateZ(atan2(it.velocity.y, it.velocity.x) + PConstants.HALF_PI)
+            rotateY(atan2(it.velocity.x, it.velocity.z) + PConstants.HALF_PI)
+            beginShape(PConstants.TRIANGLES)
+            vertex(0f, -BOID_BODY_SIZE * 2, 0f)
+            vertex(-BOID_BODY_SIZE, BOID_BODY_SIZE * 2, 0f)
+            vertex(BOID_BODY_SIZE, BOID_BODY_SIZE * 2, 0f)
+            endShape()
+            popMatrix()
+        }
     }
 
     override fun keyPressed() {
@@ -78,10 +127,5 @@ class AppDisplayManager : PApplet (){
         when(keyCode){
 
         }
-    }
-
-    companion object {
-        //フィールドの設定
-        private const val W_SIZE = 100f //立方体を想定。その中心を0としたときの面までの最短距離(つまり一辺の半分の長さ)
     }
 }
